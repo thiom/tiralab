6682aa9ff6dadda41062b9dad99ec63948cd8e14
thiom <tomi.hnn@gmail.com>
2023-04-05 23:03:12 +0300

    coverage report updated

--- rs-regex/gen_coverage.sh
+++ rs-regex/gen_coverage.sh
@@ -4,4 +4,5 @@ export RUSTFLAGS="-Cinstrument-coverage"
 cargo build
 export LLVM_PROFILE_FILE="cov-%p-%m.profraw"
 cargo test
-grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
+grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./coverage/
+echo "coverage report generated"
