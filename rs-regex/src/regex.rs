a752f291282a09500eb37431e4a4fad0d706a2b0
thiom <tomi.hnn@gmail.com>
2023-04-05 22:44:10 +0300

    some tests added

--- rs-regex/src/regex.rs
+++ rs-regex/src/regex.rs
@@ -19,3 +19,58 @@ impl Regex {
         recognizer.accepts(string.as_bytes())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::Rng;
+
+    #[test]
+    fn basic_lowercase() {
+        let regex = "aa(b|cc)*a";
+        let dfa = Regex::new(regex.to_string()).unwrap();
+        let to_accept = vec!["aaa", "aaba", "aacca", "aabba", "aabbccbbbccbcca"];
+        let to_deny = vec!["aa", "aabaa", "aaccca", "aabbac", "bbccbbbccbcca"];
+        for s in to_accept {
+            assert!(dfa.matches(s.to_string()));
+        }
+        for s in to_deny {
+            assert!(!dfa.matches(s.to_string()));
+        }
+    }
+
+    #[test]
+    fn basic_uppercase() {
+        let regex = "(B|cc|GG)*A";
+        let dfa = Regex::new(regex.to_string()).unwrap();
+        let to_accept = vec!["BA", "A", "ccGGA", "BBBA", "BBGGBBccA", "ccccccGGBGGA"];
+        let to_deny = vec!["B", "", "AA", "cccA", "AB", "BcA", "BB", "BBBBBBB", "F"];
+        for s in to_accept {
+            assert!(dfa.matches(s.to_string()));
+        }
+        for s in to_deny {
+            assert!(!dfa.matches(s.to_string()));
+        }
+    }
+
+    #[test]
+    fn random_lowercase() {
+        let regex = "(a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z)*";
+        let dfa = Regex::new(regex.to_string()).unwrap();
+        let mut to_accept = vec!["".to_string()];
+        let mut rng = rand::thread_rng();
+        let mut s_len = 10;
+        while s_len < 1000 {
+            let mut s = String::from("");
+            for i in 0..s_len {
+                let c = rng.gen_range(97..123) as u8 as char;
+                s.push(c.to_owned());
+            }
+            to_accept.push(s);
+            s_len = s_len + 100;
+        }
+        for string in to_accept {
+            assert!(dfa.matches(string.to_string()));
+        }
+    }
+}
--- rs-regex/src/scanner.rs
+++ rs-regex/src/scanner.rs
@@ -32,21 +32,36 @@ mod tests {
     use crate::tokens::{Token, TokenType};
 
     #[test]
-    fn scanner_string_token() {
-        let regex = "a";
-        let token = Token::new(TokenType::Char, "a".as_bytes()[0] as u8);
+    fn scanner_concat_token() {
+        let regex = "abc";
         let mut scanner = Scanner::new(regex.to_string());
+
+        let token_a = Token::new(TokenType::Char, 'a' as u8);
+        let token_b = Token::new(TokenType::Char, 'b' as u8);
+        let token_c = Token::new(TokenType::Char, 'c' as u8);
+        let token_eof = Token::new(TokenType::EOF, 0xff);
+
         let result = scanner.get_next_token();
-        assert_eq!(result, token);
+        assert_eq!(result, token_a);
+
+        let result = scanner.get_next_token();
+        assert_eq!(result, token_b);
+
+        let result = scanner.get_next_token();
+        assert_eq!(result, token_c);
+
+        let result = scanner.get_next_token();
+        assert_eq!(result, token_eof);
     }
 
     #[test]
     fn scanner_closure_tokens() {
         let regex = "a*b";
         let mut scanner = Scanner::new(regex.to_string());
-        let token_a = Token::new(TokenType::Char, "a".as_bytes()[0] as u8);
-        let token_star = Token::new(TokenType::Star, "*".as_bytes()[0] as u8);
-        let token_b = Token::new(TokenType::Char, "b".as_bytes()[0] as u8);
+        let token_a = Token::new(TokenType::Char, 'a' as u8);
+        let token_star = Token::new(TokenType::Star, '*' as u8);
+        let token_b = Token::new(TokenType::Char, 'b' as u8);
+        let token_eof = Token::new(TokenType::EOF, 0xff);
 
         let result = scanner.get_next_token();
         assert_eq!(result, token_a);
@@ -56,17 +71,21 @@ mod tests {
 
         let result = scanner.get_next_token();
         assert_eq!(result, token_b);
+
+        let result = scanner.get_next_token();
+        assert_eq!(result, token_eof);
     }
 
     #[test]
     fn scanner_group_and_union_tokens() {
         let regex = "a(b|a)";
         let mut scanner = Scanner::new(regex.to_string());
-        let token_a = Token::new(TokenType::Char, "a".as_bytes()[0] as u8);
-        let token_lparen = Token::new(TokenType::LeftParen, "(".as_bytes()[0] as u8);
-        let token_b = Token::new(TokenType::Char, "b".as_bytes()[0] as u8);
-        let token_union = Token::new(TokenType::Union, "|".as_bytes()[0] as u8);
-        let token_rparen = Token::new(TokenType::RightParen, ")".as_bytes()[0] as u8);
+        let token_a = Token::new(TokenType::Char, 'a' as u8);
+        let token_lparen = Token::new(TokenType::LeftParen, '(' as u8);
+        let token_b = Token::new(TokenType::Char, 'b' as u8);
+        let token_union = Token::new(TokenType::Union, '|' as u8);
+        let token_rparen = Token::new(TokenType::RightParen, ')' as u8);
+        let token_eof = Token::new(TokenType::EOF, 0xff);
 
         let result = scanner.get_next_token();
         assert_eq!(result, token_a);
@@ -85,5 +104,8 @@ mod tests {
 
         let result = scanner.get_next_token();
         assert_eq!(result, token_rparen);
+
+        let result = scanner.get_next_token();
+        assert_eq!(result, token_eof);
     }
 }
