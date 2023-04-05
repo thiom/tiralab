bac40ae7efe2ca4dd5cf6c63b80993b9db724be3
thiom <tomi.hnn@gmail.com>
2023-04-05 22:55:33 +0300

    coverage report updated

---
+++ rs-regex/gen_coverage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export RUSTFLAGS="-Cinstrument-coverage"
+cargo build
+export LLVM_PROFILE_FILE="cov-%p-%m.profraw"
+cargo test
+grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
