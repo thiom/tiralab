5d93bc37d6c1b93cef886fcd152cf33b54243757
thiom <tomi.hnn@gmail.com>
2023-04-13 19:29:39 +0300

    adding tests

--- rs-regex/gen_coverage.sh
+++ rs-regex/gen_coverage.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+rm -rf target/
+rm -r coverage/
 export RUSTFLAGS="-Cinstrument-coverage"
-cargo build
 export LLVM_PROFILE_FILE="cov-%p-%m.profraw"
+cargo build
 cargo test
-grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./coverage/
+grcov . -s . \
+--binary-path ./target/debug/ \
+-t html \
+--ignore-not-existing \
+-o ./coverage/ \
+--filter covered \
+--keep-only "src/*" \
+--ignore "src/main.rs" \
+--ignore "src/lib.rs" \
+--ignore "src/tokens.rs" \
+--ignore "src/ast.rs" \
+--ignore "*.cargo/*" \
+--excl-start "grcov-excl-start" \
+--excl-stop "grcov-excl-stop" \
+--excl-line "grcov-excl-line|#\\[derive\\(|//!"
+
 rm *.profraw
 echo "coverage report generated"
